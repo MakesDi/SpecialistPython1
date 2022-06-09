b23ca8a39064b94701623b7df801b21c310fdd12
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-09 17:18:10 +0300

    Update 03_task_while.py

--- Module2/practice/while/03_task_while.py
+++ Module2/practice/while/03_task_while.py
@@ -9,4 +9,9 @@
 # 123
 # 1234
 
-# TODO: your code here
+n = int(input("Введите количество ступенек: "))
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
