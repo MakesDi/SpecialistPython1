cf77f1bbafe9a6143a1cb19d6e212d3cf0dcf4d8
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-18 23:55:35 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -4,10 +4,23 @@
 
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
 
-def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+def distance(xa, ya, xb, yb, xc, yc):
+    if ((xb-xa)**2+(yb-ya)**2)**0.5 >= ((xc-xb)**2+(yc-yb)**2)**0.5:
+        if ((xc-xb)**2+(yc-yb)**2)**0.5 >= ((xc-xa)**2+(yc-ya)**2)**0.5:
+            return "AC"
+        else:
+            return "BC"
+    elif ((xb-xa)**2+(yb-ya)**2)**0.5 >= ((xc-xa)**2+(yc-ya)**2)**0.5:
+        return "AC"
+    else:
+        return "AB"
 
+xa = int(input("Введите координаты точки xa: "))
+ya = int(input("Введите координаты точки ya: "))
+xb = int(input("Введите координаты точки xb: "))
+yb = int(input("Введите координаты точки yb: "))
+xc = int(input("Введите координаты точки xc: "))
+yc = int(input("Введите координаты точки yc: "))
 
 # TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+print("Самый короткий отрезок:", distance(xa, ya, xb ,yb, xc, yc))
