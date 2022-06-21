4d59e6a2d9b36d6b62e003cfede68ba921922be7
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-21 17:23:03 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -3,7 +3,24 @@
 # Если по заданным точкам треугольник построить нельзя, выведите соответствующее сообщение.
 # Подсказка: для нахождения площади используйте Теорему Герона
 
-# TODO: your code here
+def triangle_area (x1, y1, x2, y2, x3, y3):
+    return abs(0.5 * ((x1-x3) * (y2-y3) - (x2-x3) * (y1-y3)))
+
+def triangle_perimeter (x1, y1, x2, y2, x3, y3):
+    return ((x2-x1)**2 + (y2-y1)**2)**0.5 + ((x3-x2)**2 + (y3-y2)**2)**0.5 + ((x3-x1)**2 + (y3-y1)**2)**0.5
+
+x1 = int(input("x1:"))
+y1 = int(input("y1:"))
+x2 = int(input("x2:"))
+y2 = int(input("y2:"))
+x3 = int(input("x3:"))
+y3 = int(input("y3:"))
+
+if triangle_area (x1, y1, x2, y2, x3, y3) == 0:
+    print("Треугольник построить нельзя!")
+else:
+    print(triangle_area (x1, y1, x2, y2, x3, y3))
+    print(triangle_perimeter (x1, y1, x2, y2, x3, y3))
 
 
 # Не забудьте протестировать вашу функцию
