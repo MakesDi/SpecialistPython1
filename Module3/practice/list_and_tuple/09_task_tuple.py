68e0dd1082a0838760968041001188a0bef0bd22
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-22 17:07:38 +0300

    Update 09_task_tuple.py

--- Module3/practice/list_and_tuple/09_task_tuple.py
+++ Module3/practice/list_and_tuple/09_task_tuple.py
@@ -1,4 +1,20 @@
 # Дано 3 кортежа заполненные произвольными целыми числами.
 # Найдите количество элементов, которые встречаются во всех трех кортежах.
 
-# TODO: your code here
+tup_one = 1, -2, 3, 4, -5, 0
+tup_two = 7, -9, 0, 1, 2, 3, -5
+tup_three = -5, 4, -7, 0, 8, 1
+
+count = 0
+tup_cross = []
+
+for num1 in range(len(tup_one)):
+    for num2 in range(len(tup_two)):
+        if tup_two[num2] == tup_one[num1]:
+            tup_cross.append(tup_two[num2])
+
+for num3 in range(len(tup_three)):
+    for num4 in range(len(tup_cross)):
+        if tup_three[num3] == tup_cross[num4]:
+            count += 1
+print("Количество элементов, которые встречаются во всех трех кортежах:", count)
