69ee323b91f3579b45c62ac3f66f7fbc838bac12
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-22 12:36:17 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,11 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+
+def fib_num(n):
+    fib_str = [0,1]
+    for num in range(n-2):
+        fib_str.append(sum(fib_str[num:(num+2)]))
+    return fib_str[n-1]
+
+n = int(input("Введите число n: "))
+
+print("n-ое число Фибоначчи:", fib_num(n))
