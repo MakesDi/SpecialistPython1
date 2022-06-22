5a287e8517988093dc9a85aa88009718020d2737
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-22 11:56:26 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,21 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def sec_to_hhmmss (sec_ammount):
+    hh = sec_ammount // 3600 
+    mm = (sec_ammount - 3600 * hh) // 60
+    ss = sec_ammount - 3600 * hh - 60 * mm
+    
+    if hh < 10:
+        hh = "0" + str(hh)
+    if mm < 10:
+        mm = "0" + str(mm)
+    if ss < 10:
+        ss = "0" + str(ss)
+
+    return (f"{hh}:{mm}:{ss}")
+
+sec_ammount = int(input("Введите количество секунд: "))
+
+print(sec_to_hhmmss(sec_ammount))
