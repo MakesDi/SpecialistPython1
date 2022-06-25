7caa27d2afef04545934cf21f996f457f33eb4a2
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-25 12:55:22 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,12 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination(num_items, items_on_page):
+    import math
+    if (num_items  - (num_items // items_on_page) * items_on_page) == 0:
+        return items_on_page
+    return num_items  - (num_items // items_on_page) * items_on_page
+
+num_items = int(input("Введите количество наименований товара: "))
+items_on_page = int(input("Сколько наименований товара помещается на странице?: "))
+print("Число объектов на последней странице:", pagination(num_items, items_on_page), "шт.")
