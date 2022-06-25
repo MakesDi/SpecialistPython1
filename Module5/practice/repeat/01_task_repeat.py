78fff465654e5cf4fc49803e9e13940529dcf2fb
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-25 12:27:22 +0300

    Update 01_task_repeat.py

--- Module5/practice/repeat/01_task_repeat.py
+++ Module5/practice/repeat/01_task_repeat.py
@@ -3,5 +3,9 @@
 # , где size - размер генерируемого списка c элементами в диапазоне от of до to.
 
 def gen_list(size, of, to):
-    pass
-
+    import random
+    randomize_list = []
+    for num in range(size):
+        randomize_list.append(random.randint(of,to))
+    return randomize_list
+print(gen_list(5,-5,10))
