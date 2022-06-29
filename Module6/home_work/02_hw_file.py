998094aa607f119babe864e54bfce74b6aa603bd
MakesDi <106873988+MakesDi@users.noreply.github.com>
2022-06-29 07:16:55 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,9 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
-with open("data/info.txt", "r") as f:
-    pass
+with open("data.txt", "r") as f: #Символ / запрещён в названии файла
+    s = 0
+    for line in f:
+        if line.rstrip().isdigit():
+            s += int(line)
+print(s)
